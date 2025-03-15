0096d9922b9dee056a7caba7c1c43837f766e3f2
GeunEda <144422743+geuneda@users.noreply.github.com>
2025-03-15 20:06:40 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) {
+    int answer = 0;
+    int currentYear = 2023;
+    
+    answer = currentYear - age;
+    return answer;
+}
