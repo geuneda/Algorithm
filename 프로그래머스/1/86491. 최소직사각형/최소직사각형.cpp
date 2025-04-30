e1fbb7579a8c0809546f67c6b826d8159f87b0ad
GeunEda <144422743+geuneda@users.noreply.github.com>
2025-04-30 17:49:32 +0900

    [level 1] Title: 최소직사각형, Time: 0.66 ms, Memory: 5.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+
+    int width = 0, height = 0;
+    for (int i = 0; i < sizes.size(); i++)
+    {
+        if (sizes[i][0] < sizes[i][1])
+        {
+            int temp = sizes[i][0];
+            sizes[i][0] = sizes[i][1];
+            sizes[i][1] = temp;
+        }
+        if (width < sizes[i][0])
+        {
+            width = sizes[i][0];
+        }
+        if (height < sizes[i][1])
+        {
+            height = sizes[i][1];
+        }
+    }
+    answer = width * height;
+    
+    return answer;
+}
